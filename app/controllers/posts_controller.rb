009ca26ec1f90991d3d3da833ca4fcec151bd9fe
Dan Engle <dan@griptechnology.com>
2010-04-18 23:16:47 -0500

    got rss feed working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,17 @@
 class PostsController < ApplicationController
-  
+  respond_to :html, :xml, :json, :rss
   # GET /posts
   # GET /posts.xml
   def index
     @posts = Post.published.paginate(:page => params[:page], :per_page => 3)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @posts }
-    end
+    respond_with(@posts)
   end
 
   # GET /posts/1
   # GET /posts/1.xml
   def show
     @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @post }
-    end
+    respond_with(@post)
   end
 
   # GET /posts/new
@@ -27,10 +19,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     # @comment = @post.comments.new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
-    end
+    respond_with(@post)
   end
 
   # POST /posts
--- app/views/posts/index.rss.builder
+++ app/views/posts/index.rss.builder
@@ -3,12 +3,14 @@ xml.rss :version => "2.0" do
   xml.channel do
     xml.title "Posts"
     xml.description "Keep up do date with all the latest posts"
-    xml.link formatted_posts_url(:rss)
+    xml.link posts_url(:rss)
   end
   @posts.each do |post|
-    xml.post do
-      xml.body post.body
+    xml.item do
+      xml.title post.id
+      xml.description post.body
       xml.pubDate post.published_at.to_s(:rfc822)
+      xml.link post_url(post, :rss)
     end
   end
 end
