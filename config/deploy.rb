bc093cf598db433cb83cf7d803fca742e70fce07
Dan Engle <dan@griptechnology.com>
2010-06-19 19:06:50 -0500

    capistrano and db stuff

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "fml_clone"
+set :repository,  "git@github.com:danengle/fml_clone.git"
+set :deploy_to, "/home/danengle/fml.danengle.us"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "danengle.us"                          # Your HTTP server, Apache/etc
+role :app, "danengle.us"                          # This may be the same as your `Web` server
+role :db,  "danengle.us", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+set :user, "danengle"
+set :scm_username, "danengle"
+set :use_sudo, false
+
+set :default_environment, { 
+  'PATH' => "/home/danengle/.rvm/rubies/ruby-1.9.2-preview3/bin:/home/danengle/.rvm/gems/ruby-1.9.2-preview3/bin:/home/danengle/.rvm/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.2',
+  'GEM_HOME' => '/home/danengle/.rvm/gems/ruby-1.9.2-preview3/gems',
+  'GEM_PATH' => '/home/danengle/.rvm/gems/ruby-1.9.2-preview3/gems' 
+}
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
